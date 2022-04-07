8140ae5c1528138d67c383646defd33b2a9fa08f
rlw92 <r_l_williams@yahoo.com>
2022-04-07 18:23:42 +0100

    Set up webpack workflow, set up the three tabbed js pages. Project is ready to be coded.

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(()=>{let e=document.createElement("div");e.style="background-color:green;text-align:center;font-size:100px;",e.textContent="Pasta Paradise",document.querySelector("#content").appendChild(e)})()})();
